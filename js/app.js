28066f5ccb8b0d453dde47a05d90299124c9022f
Patrik-Hasselbad <phasselblad@hotmail.com>
2019-12-09 23:00:42 +0100

    Adding small data changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// Kolla upp om det går att gömma browserns meny, sökfält osv i ett popup fönster. Kolla också om det är tillåtet med popuppisar för att göra sin app.
+/* Kolla upp om det går att gömma browserns meny, sökfält osv i ett popup fönster. Kolla också om det är tillåtet med popuppisar för att göra sin app.
+
+ATT GÖRA:
+*Skapa componenter - alltså det där shadowroot skiten.
+*Börja planera för klockuppgiften.
+*Gör Memory modulen, ja först memory övningen förstås.
+*Gör små ikoner som föreställer chat, memory och klockappen (till att börja med).
+
+*/
