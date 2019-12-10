391b4486d8bdac30a0de9798d95195c80adaaba0
Patrik-Hasselbad <phasselblad@hotmail.com>
2019-12-10 22:27:59 +0100

    Adding Memory constructor

--- js/NewMemory.js
+++ js/NewMemory.js
@@ -3,3 +3,20 @@
  * @module ./src/js/NewMemory.js
  * @version 1.0
  */
+
+/**
+ * Constructor to create a new Memory session.
+ * @param {Number} rows - Number of rows in game.
+ * @param {Number} cols - Number of columns in game.
+ * @param {String} container - Location where the memory should be shown.
+ * @exports {Object} - A new Object is exported.
+ */
+class NewMemory {
+  constructor (rows, cols, container) {
+    this.rows = rows
+    this.cols = cols
+    this.container = container
+  }
+}
+
+export { NewMemory }
