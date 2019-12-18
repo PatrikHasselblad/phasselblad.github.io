cab5dd39336c6e8ec5283e5480cd0fae6fdf68a0
Patrik-Hasselbad <phasselblad@hotmail.com>
2019-12-18 05:36:58 +0100

    Adding work on draggable div

--- js/desktop.js
+++ js/desktop.js
@@ -8,6 +8,16 @@ import { Memory } from './Memory.js'
 import { Chat } from './Chat.js'
 import { LearnClock } from './LearnClock.js'
 
+const template = document.createElement('template')
+template.innerHTML = `
+<style>
+:host #desktopApp {
+  width: 100%;
+  height: 100%;
+}
+<style>
+`
+
 /**
  * Class to listen and launch requested apps and handle global functions.
  * @constructor Constructs a shadow DOM where new instances of apps are executed in their own sub-shadow DOM.
@@ -17,25 +27,95 @@ class Desktop extends window.HTMLElement {
     super()
 
     this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
 
     // Initiates a new memory-game
     const memoryBtn = document.querySelector('#memorybtn')
     memoryBtn.addEventListener('click', e => {
       e.preventDefault()
       this.shadowRoot.appendChild(new Memory())
+      const box = this.shadowRoot.querySelector('memory-app')
+      this.draggableWindow(box)
     })
     // Initiates a new chat-window
     const chatBtn = document.querySelector('#chatbtn')
     chatBtn.addEventListener('click', e => {
       e.preventDefault()
       this.shadowRoot.appendChild(new Chat())
+      const box = this.shadowRoot.querySelector('chat-app')
+      this.draggableWindow(box)
     })
     // Initiates a new clock-exercise
     const clockBtn = document.querySelector('#clockbtn')
     clockBtn.addEventListener('click', e => {
       e.preventDefault()
       this.shadowRoot.appendChild(new LearnClock())
+      const box = this.shadowRoot.querySelector('learnclock-app')
+      this.draggableWindow(box)
     })
+  }
+
+  connectedCallback () {
+  }
+
+  draggableWindow (divBox) {
+    const box = divBox
+    // const container = document.querySelector('.wrapper')
+
+    let active = false
+    let currentX
+    let currentY
+    let initialX
+    let initialY
+    let xOffset = 0
+    let yOffset = 0
+
+    box.addEventListener('touchstart', dragStart, { passive: true })
+    box.addEventListener('touchend', dragEnd, false)
+    box.addEventListener('touchmove', drag, { passive: true })
+    box.addEventListener('mousedown', dragStart, false)
+    box.addEventListener('mouseup', dragEnd, false)
+    box.addEventListener('mousemove', drag, false)
+
+    function dragStart (e) {
+      if (e.type === 'touchstart') {
+        initialX = e.touches[0].clientX - xOffset
+        initialY = e.touches[0].clientY - yOffset
+      } else {
+        initialX = e.clientX - xOffset
+        initialY = e.clientY - yOffset
+      }
+      if (e.target === box) {
+        active = true
+      }
+      console.log(currentX, currentY)
+    }
+    function dragEnd (e) {
+      initialX = currentX
+      initialY = currentY
+
+      active = false
+    }
+    function drag (e) {
+      if (active) {
+        // e.preventDefault()
+
+        if (e.type === 'touchmove') {
+          currentX = e.touches[0].clientX - initialX
+          currentY = e.touches[0].clientY - initialY
+        } else {
+          currentX = e.clientX - initialX
+          currentY = e.clientY - initialY
+        }
+        xOffset = currentX
+        yOffset = currentY
+
+        setTranslate(currentX, currentY, box)
+      }
+    }
+    function setTranslate (xPos, yPos, element) {
+      element.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0)'
+    }
   }
 }
 
