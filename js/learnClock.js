57295d545150cb519b749e6d11776d2a5f46c977
Patrik-Hasselbad <phasselblad@hotmail.com>
2019-12-18 21:53:03 +0100

    Adding buggy, but working clock

--- js/learnClock.js
+++ js/learnClock.js
@@ -55,8 +55,12 @@ class LearnClock extends window.HTMLElement {
     let radius = canvas.height / 2
     ctx.translate(radius, radius)
     radius = radius * 0.90
-    drawClock()
+    // drawClock()
+    setInterval(drawClock, 1000)
 
+    /**
+     * Function to draw clock cirles and add color to them.
+     */
     function drawClock () {
       ctx.arc(0, 0, radius, 0, 2 * Math.PI)
       ctx.fillStyle = 'white'
@@ -75,6 +79,50 @@ class LearnClock extends window.HTMLElement {
       ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI)
       ctx.fillStyle = '#333'
       ctx.fill()
+      drawNumbers()
+    }
+    function drawNumbers () {
+      ctx.font = radius * 0.15 + 'px arial'
+      ctx.textBaseline = 'middle'
+      ctx.textAlign = 'center'
+
+      for (let num = 1; num < 13; num++) {
+        const ang = num * Math.PI / 6
+        ctx.rotate(ang)
+        ctx.translate(0, -radius * 0.85)
+        ctx.rotate(-ang)
+        ctx.fillText(num.toString(), 0, 0)
+        ctx.rotate(ang)
+        ctx.translate(0, radius * 0.85)
+        ctx.rotate(-ang)
+      }
+      drawTime()
+    }
+    function drawTime () {
+      const now = new Date()
+      let hour = now.getHours()
+      let minute = now.getMinutes()
+      let second = now.getSeconds()
+
+      hour = hour % 12
+      hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (360 * 60))
+      drawHand(ctx, hour, radius * 0.5, radius * 0.07)
+
+      minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60))
+      drawHand(ctx, minute, radius * 0.8, radius * 0.07)
+
+      second = (second * Math.PI / 30)
+      drawHand(ctx, second, radius * 0.9, radius * 0.02)
+    }
+    function drawHand (ctx, pos, length, width) {
+      ctx.beginPath()
+      ctx.lineWidth = width
+      ctx.lineCap = 'round'
+      ctx.moveTo(0, 0)
+      ctx.rotate(pos)
+      ctx.lineTo(0, -length)
+      ctx.stroke()
+      ctx.rotate(-pos)
     }
   }
 }
