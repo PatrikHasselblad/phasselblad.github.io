6c3066ac70d58efba6a4f78c34fdd6fd56c6984f
Patrik-Hasselbad <phasselblad@hotmail.com>
2019-12-19 23:16:57 +0100

    Adding code for clock exercise result

--- js/learnClock.js
+++ js/learnClock.js
@@ -65,18 +65,26 @@ class LearnClock extends window.HTMLElement {
    */
   initializeClock () {
     const canvas = this.shadowRoot.querySelector('#clockCanvas')
+    const hourAnswer = this.shadowRoot.querySelector('#hourIn')
+    const minAnswer = this.shadowRoot.querySelector('#minIn')
     const ctx = canvas.getContext('2d')
     let radius = canvas.height / 2
     ctx.translate(radius, radius)
     radius = radius * 0.90
+    let hour = 0
+    let minute = 0
+    let hourReal = 0
+    let minReal = 0
     drawClock(ctx, radius)
 
-    // Check if answer is correct and continues. -----------------------Also, add answer if it is correct.
+    // Check if answer is correct and continues. -----------------------Also, add answer if it is correct. compare arrays perhaps, easier than strings, dont forget to remove zero's if any.
     const updateClock = this.shadowRoot.querySelector('#clockAnswerBtn')
     updateClock.addEventListener('click', e => {
       e.preventDefault()
+      answerCheck()
       drawClock()
     })
+
     /**
      * Function to draw clock cirles and add color to them.
      */
@@ -151,9 +159,11 @@ class LearnClock extends window.HTMLElement {
       let minute = now.getMinutes() // -----------------registrerar tiden, - sekund, sedan jämför vi svaret med strängen, om fel
       let second = now.getSeconds() // -----------------'oj vad synd' och visar korrekt tid och svar, annars 'Bra jobbat å 1 poäng.
       */
-      let hour = Math.floor(Math.random() * (13 - 1) + 1)
-      let minute = Math.floor(Math.random() * (60 - 1) + 1)
+      hour = Math.floor(Math.random() * (13 - 1) + 1)
+      minute = Math.floor(Math.random() * (60 - 1) + 1)
       console.log('Tim: ' + hour + ' Min: ' + minute)
+      hourReal = hour
+      minReal = minute
 
       hour = hour % 12
       hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) // + (second * Math.PI / (360 * 60))
@@ -176,10 +186,59 @@ class LearnClock extends window.HTMLElement {
       ctx.stroke()
       ctx.rotate(-pos)
     }
+    function answerCheck () {
+      const answerTag = this.shadowRoot.querySelector('#returnAnswer')
+      console.log(hourReal, minReal)
+      let hour = hourAnswer.value
+      let min = minAnswer.value
+      let newHour = hourReal.toString()
+      let newMin = minReal.toString()
+
+      // If no input value, add a 0.
+      if (hour.length === 0) {
+        hour = '0'
+      }
+      if (min.length === 0) {
+        min = '0'
+      }
+
+      // For better printout, add 0 if single int.
+      if (newHour.length === 1) {
+        newHour = ('0' + newHour)
+      }
+      if (newMin.length === 1) {
+        newMin = (newMin + '0')
+      }
+      if (hour.length === 1) {
+        hour = ('0' + hour)
+      }
+      if (min.length === 1) {
+        min = (min + '0')
+      }
+
+      console.log('Klockan är ' + hour + ':' + min)
+      const correctTime = 'Korrekt svar skulle ha varit ' + newHour + ':' + newMin
+
+      if (hour === newHour && min === newMin) {
+        console.log('Correct!')
+        answerTag.innerText = 'Correct!'
+        returnAnswer(answerTag)
+      } else {
+        console.log(correctTime)
+        answerTag.innerText = correctTime
+        returnAnswer(answerTag)
+      }
+      hourAnswer.value = ''
+      minAnswer.value = ''
+    }
+
+    function returnAnswer (answerTag) {
+      this.shadowRoot.appendChild(answerTag)
+    }
   }
 }
 
+// Användaren ska kunna välja antalet övningar, alltså hur många gånger man behöver svara på olika klockslag.
+
 window.customElements.define('learnclock-app', LearnClock)
 export { LearnClock }
-// Användaren ska kunna välja antalet övningar, alltså hur många gånger man behöver svara på olika klockslag.
-// Random tim, random min, gör om till enskilda heltal. Användaren ska svara 7.30, eller 07.30, kanske i separata rutor för tim å min.
