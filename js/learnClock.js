c133e4e718c36d1982c871d3f53125181a5e42ce
Patrik-Hasselbad <phasselblad@hotmail.com>
2019-12-19 14:19:15 +0100

    Trying to add lines for seconds

--- js/learnClock.js
+++ js/learnClock.js
@@ -54,7 +54,7 @@ class LearnClock extends window.HTMLElement {
     let radius = canvas.height / 2
     ctx.translate(radius, radius)
     radius = radius * 0.90
-    setInterval(drawClock, 10000)
+    setInterval(drawClock, 1000)
 
     /**
      * Function to draw clock cirles and add color to them.
@@ -90,6 +90,7 @@ class LearnClock extends window.HTMLElement {
       ctx.font = radius * 0.15 + 'px arial'
       ctx.textBaseline = 'middle'
       ctx.textAlign = 'center'
+      const secondHandLength = 60
 
       for (let num = 1; num < 13; num++) {
         const ang = num * Math.PI / 6
@@ -100,6 +101,23 @@ class LearnClock extends window.HTMLElement {
         ctx.rotate(ang)
         ctx.translate(0, radius * 0.85)
         ctx.rotate(-ang)
+      }
+
+      for (let i = 0; i < 60; i++) {
+        const ang = (i - 3) * (Math.PI * 2) / 60
+        ctx.lineWidth = 1
+        ctx.beginPath()
+
+        const x1 = (canvas.width / 2) + Math.cos(ang) * (secondHandLength)
+        const y1 = (canvas.height / 2) + Math.sin(ang) * (secondHandLength)
+        const x2 = (canvas.width / 2) + Math.cos(ang) * (secondHandLength - (secondHandLength / 30))
+        const y2 = (canvas.height / 2) + Math.sin(ang) * (secondHandLength - (secondHandLength / 30))
+
+        ctx.moveTo(x1, y1)
+        ctx.lineTo(x2, y2)
+
+        ctx.strokeStyle = '#000'
+        ctx.stroke()
       }
     }
 
